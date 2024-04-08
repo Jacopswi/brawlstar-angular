54a414378e3f6ec1026cc21aceed375f42721ebb
BONNET Maxime <maxime.bonnet6@etu.univ-lorraine.fr>
2024-04-08 19:55:22 +0200

    tris

--- projet_final/src/app/brawler/brawler.component.ts
+++ projet_final/src/app/brawler/brawler.component.ts
@@ -1,15 +1,58 @@
 import { Component, OnInit } from '@angular/core';
-import { JsonDataService } from '../services/brawl-stars.service';
-import brawlers from '../../assets/API_Brawlers.json'
+import brawlers from '../../assets/API_Brawlers.json';
 
 @Component({
   selector: 'app-brawler',
   templateUrl: './brawler.component.html',
-  styleUrl: './brawler.component.css'
+  styleUrls: ['./brawler.component.css']
 })
-export class BrawlerComponent{
+export class BrawlerComponent implements OnInit {
+  public listeBrawlers: any[] = [];
+  public sortedBrawlers: any[] = [];
 
-  public listeBrawlers = brawlers
+  constructor() {}
+
+  ngOnInit() {
+    // Initialiser les données ici, par exemple :
+    this.listeBrawlers = brawlers;
+    this.sortedBrawlers = this.listeBrawlers[0].items; 
+  }
+
+  sortBrawlers() {
+    // Tri par nom croissant
+    this.sortedBrawlers.sort((a, b) => {
+      const nameA = a.name.toLowerCase();
+      const nameB = b.name.toLowerCase();
+      if (nameA < nameB) return -1;
+      if (nameA > nameB) return 1;
+      return 0;
+    });
+  }
+
+  sortBrawlersReverse() {
+    // Tri par nom croissant
+    this.sortedBrawlers.sort((a, b) => {
+      const nameA = a.name.toLowerCase();
+      const nameB = b.name.toLowerCase();
+      if (nameB < nameA) return -1;
+      if (nameB > nameA) return 1;
+      return 0;
+    });
+
+  
+  }
 
 
+  sortId() {
+    // Tri par nom croissant
+    this.sortedBrawlers.sort((a, b) => {
+      const Id1 = a.id;
+      const Id2 = b.id;
+      if (Id1 < Id2) return -1;
+      if (Id1 > Id2) return 1;
+      return 0;
+    });
+
+  
+  }
 }
