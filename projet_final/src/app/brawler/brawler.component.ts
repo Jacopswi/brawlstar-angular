01c058e90397db8931d38d83a665a13ee1a58b4c
BONNET Maxime <maxime.bonnet6@etu.univ-lorraine.fr>
2024-04-08 02:53:22 +0200

    affichage data qui marche

--- projet_final/src/app/brawler/brawler.component.ts
+++ projet_final/src/app/brawler/brawler.component.ts
@@ -1,20 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { JsonDataService } from '../services/brawl-stars.service';
+import brawlers from '../../assets/API_Brawlers.json'
 
 @Component({
   selector: 'app-brawler',
   templateUrl: './brawler.component.html',
   styleUrl: './brawler.component.css'
 })
-export class BrawlerComponent implements OnInit{
-  brawlStarsData: any[] = [];
+export class BrawlerComponent{
 
-  constructor(private brawlStarsService: JsonDataService) { }
+  public listeBrawlers = brawlers
 
-  ngOnInit(): void {
-    this.brawlStarsService.getData().subscribe(data => {
-      this.brawlStarsData = data;
-    });
-  }
 
 }
