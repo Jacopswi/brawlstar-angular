c235ea239b8b8bd9b514b207243c2ddce9dd820c
unknown <jacopswi54@gmail.com>
2024-04-08 21:20:21 +0200

    ajouts rareté + tris + background

--- projet_final/src/app/brawler/brawler.component.ts
+++ projet_final/src/app/brawler/brawler.component.ts
@@ -52,7 +52,74 @@ export class BrawlerComponent implements OnInit {
       if (Id1 > Id2) return 1;
       return 0;
     });
-
-  
   }
+
+  getRarityClass(rarity: string): string {
+    // Déterminer la classe CSS en fonction de la rareté du Brawler
+    switch (rarity) {
+      case 'common':
+        return 'common-bg';
+
+      case 'rare':
+        return 'epic-bg';
+
+      case 'super_rare':
+        return 'super_rare-bg';
+
+      case 'epic':
+        return 'epic-bg';
+
+      case 'mythic':
+        return 'mythic-bg';
+
+      case 'legendary':
+        return 'legendary-bg';
+
+
+
+      default:
+        return '';
+    }
+  }
+
+
+  sortRarity() {
+    const rarityMapping: { [key: string]: number } = {
+      'common': 1,
+      'rare': 2,
+      'super_rare': 3,
+      'epic': 4,
+      'mythic': 5,
+      'legendary': 6
+    };
+  
+    this.sortedBrawlers.sort((a, b) => {
+      const rarityA = rarityMapping[a.rarity.toLowerCase()] || Number.MAX_SAFE_INTEGER;
+      const rarityB = rarityMapping[b.rarity.toLowerCase()] || Number.MAX_SAFE_INTEGER;
+  
+      return rarityA - rarityB;
+    });
+  }
+
+
+  sortRarityReverse() {
+    const rarityMapping: { [key: string]: number } = {
+      'common': 6,
+      'rare': 5,
+      'super_rare': 4,
+      'epic': 3,
+      'mythic': 2,
+      'legendary': 1
+    };
+  
+    this.sortedBrawlers.sort((a, b) => {
+      const rarityA = rarityMapping[a.rarity.toLowerCase()] || Number.MAX_SAFE_INTEGER;
+      const rarityB = rarityMapping[b.rarity.toLowerCase()] || Number.MAX_SAFE_INTEGER;
+  
+      return rarityA - rarityB;
+    });
+  }
+
+
+
 }
