5015e6cc3a18274644788fe8417cca9cb2b748eb
unknown <jacopswi54@gmail.com>
2024-04-09 23:00:38 +0200

    affichage fonctionnel modes de jeu

--- projet_final/src/app/joueur/joueur.component.ts
+++ projet_final/src/app/joueur/joueur.component.ts
@@ -8,6 +8,9 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class JoueurComponent implements OnInit{
   gamemodes: any;
+  public listeModes: any[] = [];
+  public sortedModes: any[] = [];
+  public searchTerm : string = "";
 
   constructor(private http: HttpClient) { }
 
@@ -15,6 +18,50 @@ export class JoueurComponent implements OnInit{
     this.http.get('https://api.brawlapi.com/v1/gamemodes').subscribe(data => {
       this.gamemodes = data;
       console.log(this.gamemodes); 
+      this.listeModes = this.gamemodes.list
+      this.sortedModes = this.listeModes
     });
+  }
+
+  
+  sortModes() {
+    this.sortedModes.sort((a, b) => {
+      const nameA = a.name.toLowerCase();
+      const nameB = b.name.toLowerCase();
+      if (nameA < nameB) return -1;
+      if (nameA > nameB) return 1;
+      return 0;
+    });
+  }
+
+  sortModesReverse() {
+    this.sortedModes.sort((a, b) => {
+      const nameA = a.name.toLowerCase();
+      const nameB = b.name.toLowerCase();
+      if (nameB < nameA) return -1;
+      if (nameB > nameA) return 1;
+      return 0;
+    });
+
+  
+  }
+  sortId() {
+    this.sortedModes.sort((a, b) => {
+      const Id1 = a.id;
+      const Id2 = b.id;
+      if (Id1 < Id2) return -1;
+      if (Id1 > Id2) return 1;
+      return 0;
+    });
+  }
+
+  filterModes(): any[] {
+    return this.sortedModes.filter(mode =>
+      mode.name.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
+  }
+
+  updateSearchTerm(event: any): void {
+    this.searchTerm = event.target.value;
   }
 }
